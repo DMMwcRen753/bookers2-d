fe8da8a62992ec7f99eea2f97acfc278d15cd2ef
RenSaitou <112914054+DMMwcRen753@users.noreply.github.com>
2022-11-05 12:01:32 +0000

    [add]star

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,6 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -37,7 +36,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
